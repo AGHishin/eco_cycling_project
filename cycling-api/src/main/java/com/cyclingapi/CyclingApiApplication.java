8f4c2f5d2884522a779b823c3782b0820bd879dd
aghilas haddouche <haddoucheaghilas@gmail.com>
2021-02-21 23:03:09 +0100

    initial commit

---
+++ cycling-api/src/main/java/com/cyclingapi/CyclingApiApplication.java
@@ -0,0 +1,13 @@
+package com.cyclingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyclingApiApplication.class, args);
+	}
+
+}
---
+++ cycling-api/src/test/java/com/cyclingapi/CyclingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyclingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyclingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
